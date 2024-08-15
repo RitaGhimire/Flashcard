b0e1ca23d91ddfb725bb716b90c22a707631bb85
Sushant <sdahal249@gmail.com>
2024-08-14 20:10:23 -0400

    right commit

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,35 +1,35 @@
-import { AppBar, Container, Toolbar, Typography,Button,Link,Box} from "@mui/material";
-import { SignIn} from "@clerk/nextjs";
+import { AppBar, Container, Toolbar, Typography, Button, Box } from "@mui/material";
+import NextLink from "next/link"; // Use Next.js Link for routing
+import { SignIn } from "@clerk/nextjs";
 
+export default function SignInPage() {
+  return (
+    <Container maxWidth="sm">
+      <AppBar position="static" sx={{ backgroundColor: "#3f51b5" }}>
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Flashcard SaaS
+          </Typography>
+          <NextLink href="/sign-in" passHref>
+            <Button color="inherit">Login</Button>
+          </NextLink>
+          <NextLink href="/sign-up" passHref>
+            <Button color="inherit">Sign Up</Button>
+          </NextLink>
+        </Toolbar>
+      </AppBar>
 
-
-export default function signUpPage(){
-    return (<Container maxWidth = "sm">
-        <AppBar position = "static" sx = {{backgroundColor: "#3f51b5"}}>
-<Toolbar>
-    <Typography
-     variant = 'h6' 
-     sx = {{flexGrow: 1,
-     }}> 
-     Flashcard Saas 
-     </Typography>
-    <Button color = "inherit" href = "/sign-in">
-        <Link href = "/sign-in" passhref>Login</Link>
-    </Button>
-    <Button color = "inherit" href = "/sign-in">
-        <Link href = "/sign-up" passhref>Sign Up</Link>
-    </Button>
-</Toolbar>
-        </AppBar>
-{/* /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
-<Box
-    display = "flex"
-    flexDirection = "column"
-    alignItems = "center"
-    justifyContent = "center">
-        <Typography variant = "h4">Sign In</Typography>
+      {/* Main Content */}
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ mt: 4 }}
+      >
+        <Typography variant="h4">Sign In</Typography>
         <SignIn />
-</Box>
+      </Box>
     </Container>
-    ) 
+  );
 }
--- app/sign-up/[[...sign-up]]/page.js
+++ app/sign-up/[[...sign-up]]/page.js
@@ -1,35 +1,39 @@
-import { AppBar, Container, Toolbar, Typography,Button,Link,Box} from "@mui/material";
-import { SignUp} from "@clerk/nextjs";
+import { AppBar, Container, Toolbar, Typography, Button, Box } from "@mui/material";
+import NextLink from "next/link"; // Use Next.js Link for routing
+import { SignUp } from "@clerk/nextjs";
 
+export default function SignUpPage() {
+  return (
+    <Container maxWidth="sm">
+      <AppBar position="static" sx={{ backgroundColor: "#3f51b5" }}>
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Flashcard SaaS
+          </Typography>
+          <NextLink href="/sign-in" passHref>
+            <Button color="inherit" component="a">
+              Login
+            </Button>
+          </NextLink>
+          <NextLink href="/sign-up" passHref>
+            <Button color="inherit" component="a">
+              Sign Up
+            </Button>
+          </NextLink>
+        </Toolbar>
+      </AppBar>
 
-
-export default function signUpPage(){
-    return (<Container maxWidth = "sm">
-        <AppBar position = "static" sx = {{backgroundColor: "#3f51b5"}}>
-<Toolbar>
-    <Typography
-     variant = 'h6' 
-     sx = {{flexGrow: 1,
-     }}> 
-     Flashcard Saas 
-     </Typography>
-    <Button color = "inherit" href = "/sign-in">
-        <Link href = "/sign-in" passhref>Login</Link>
-    </Button>
-    <Button color = "inherit" href = "/sign-up">
-        <Link href = "/sign-up" passhref>Sign Up</Link>
-    </Button>
-</Toolbar>
-        </AppBar>
-{/* /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
-<Box
-    display = "flex"
-    flexDirection = "column"
-    alignItems = "center"
-    justifyContent = "center">
-        <Typography variant = "h4">Sign Up</Typography>
+      {/* Main Content */}
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ mt: 4 }}
+      >
+        <Typography variant="h4">Sign Up</Typography>
         <SignUp />
-</Box>
+      </Box>
     </Container>
-    ) 
+  );
 }
